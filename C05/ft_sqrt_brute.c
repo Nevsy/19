54eaaa0a197f54d664f96173784aed7b6afef938
Nevsy <sven@royer.se>
2024-07-16 09:43:31 +0200

    sqrt modifs ...

--- C05/ft_sqrt_brute.c
+++ C05/ft_sqrt_brute.c
@@ -1,17 +1,19 @@
 int	ft_sqrt(int nb)
 {
-	int	i;
-	int	cap;
+	int		i;
+	int		cap;
+	long	nb_cpy;
 
+	nb_cpy = nb;
 	i = 0;
-	if (nb <= 0)
+	if (nb_cpy <= 0)
 		return (0);
-	else if (nb == 1)
+	else if (nb_cpy == 1)
 		return (1);
-	cap = (nb - 1) / 2 + 1;
-	while (nb > i * i && i < cap)
+	cap = (nb_cpy - 1) / 2 + 1;
+	while (nb_cpy > i * i && i < cap)
 		i++;
-	if (nb == i * i)
+	if (nb_cpy == i * i)
 		return (i);
 	return (0);
 }
@@ -19,7 +21,7 @@ int	ft_sqrt(int nb)
 #include <stdio.h>
 int	main(void)
 {
-	printf("%d\n", ft_sqrt(1));
+	printf("%d\n", ft_sqrt(43630 * 43630));
 	printf("%d\n", ft_sqrt(25));
 	printf("%d\n", ft_sqrt(4330 * 4330));
 	printf("%d\n", ft_sqrt(4330 * 4330 + 1));
